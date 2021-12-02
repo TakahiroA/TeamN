58d63909d9bfab90f1024b662c3b94c8668af504
github <github>
2021-12-02 16:03:22 +0900

    合わせた

--- N/static/js/modal.js
+++ N/static/js/modal.js
@@ -1,17 +1,17 @@
-// タスク追加モーダル管理
+// タスク詳細モーダル管理
 $(function () {
 
     //追加ボタンを押した際
-    $('#CTB').click(function () {
+    $('#TDI').click(function () {
         //背景を付ける
         $("body").append('<div id="modal-bg"></div>');
 
         //モーダルウィンドウを表示
-        $("#modal-bg,#modal-task-add").fadeIn("fast");
+        $("#modal-bg,#modal-task-details").fadeIn("fast");
 
         //画面の背景もしくは戻るボタンを押したらモーダルを閉じる
-        $('#modal-bg, #CTBC').click(function () {
-            $("#modal-bg,#modal-task-add").fadeOut("fast", function () {
+        $('#modal-bg, #CTDI').click(function () {
+            $("#modal-bg,#modal-task-details").fadeOut("fast", function () {
                 $('#modal-bg').remove();
             });
         });
